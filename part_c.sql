6cc65a378b2616f3b6336d05e2d17300b84fd48c
michaelwalker <michael.walker@officespark.com>
2018-07-20 04:41:40 +1000

    forth

--- part_c.sql
+++ part_c.sql
@@ -2,6 +2,7 @@
 CREATE TABLE PURCHASES AS
 select
 	count(case when customer_name = 'John Smith' then 1 end) johns_purchases,
-	count(case when customer_name = 'David Kim' then 1 end) davids_purchases
+	count(case when customer_name = 'David Kim' then 1 end) davids_purchases,
+	count(case when customer_name = 'Mary Jane' then 1 end) marys_purchases
   from
 	CUSTOMER_SALES_PRODUCTS;
