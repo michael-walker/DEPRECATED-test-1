a63508f645bb58aa4c853506315e3d3fda508c65
michaelwalker <michael.walker@officespark.com>
2018-07-20 16:03:13 +1000

    silly commit 2

--- part_c.sql
+++ part_c.sql
@@ -1,8 +1,8 @@
 
-CREATE TABLE PURCHASES AS jkljklj
+CREATE TABLE PURCHASES AS
 select
 	count(case when customer_name = 'John Smith' then 1 end) johns_purchases,
 	count(case when customer_name = 'David Kim' then 1 end) davids_purchases,
 	count(case when customer_name = 'Mary Jane' then 1 end) marys_purchases
   from
-	CUSTOMER_SALES_PRODUCTS;
+	CUSTOMER_SALES_PRODUCTS_ABC;
